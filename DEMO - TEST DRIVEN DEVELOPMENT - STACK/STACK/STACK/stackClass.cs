c4167b350617120f2c7e2f748ea1a9adee69dfd2
Rifath <rifath.ahmed@gocompare.com>
2020-02-26 22:04:09 +0000

    Testing Demo 26th Feb

---
+++ DEMO - TEST DRIVEN DEVELOPMENT - STACK/STACK/STACK/stackClass.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stack
+{
+    public class Stack<T>
+    {
+        // https://www.freecodecamp.org/news/tdd-explanation-hands-on-practice-with-c-a0124338be44/
+
+        // Members
+        private T[] stackArray;
+        private int maximumLength;
+
+        // Properties
+        public int Size { get; private set; }
+
+        // Constructor
+        public Stack (int length)
+        {
+            maximumLength = length;
+            stackArray = new T[length];
+        }
+    }
+}
---
+++ DEMO - TEST DRIVEN DEVELOPMENT - STACK/STACK/StackTests/StackTest.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackTests
+{
+    [TestFixture]
+    public class StackTests
+    {
+        [Test]
+        public void Creation()
+        {
+            Stack<int> s = new Stack<int>(3);
+            Assert.AreEqual(0, s.Size);
+        }
+    }
+}
